c53a277676c8087626770d6c1db17a7a8cab2e8a
Alfred Crosby <alfredcrozby@gmail.com>
2017-10-04 02:27:48 +0700

    remove unused files

--- client/routes.js
+++ client/routes.js
@@ -3,7 +3,6 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import NewPost from './pages/NewPost';
-import EditPost from './pages/EditPost';
 import Post from './pages/Post';
 import NotFound from './pages/NotFound';
 
@@ -11,7 +10,6 @@ const routes = (
   <Switch>
     <Route exact path="/" component={Home} />
     <Route path="/p/create" component={NewPost} />
-    <Route path="/p/edit/:slug" component={EditPost} />
     <Route path="/p/:slug" component={Post} />
     <Route component={NotFound} />
   </Switch>
